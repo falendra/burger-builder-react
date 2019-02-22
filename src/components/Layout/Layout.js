08a744b75ecf29e48927fe0d361b62ce3f0ba702
Falendra Bandhe <sahilbandhe@gmail.com>
2019-02-23 03:03:24 +0530

    HOC and layout added

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => (
+    <Aux>
+
+        <div>ToolBar , SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+
+    </Aux>
+
+
+);
+
+export default layout;
