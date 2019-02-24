a3d8d21764113ee5531d9ea3f56746ef5a358892
Falendra Bandhe <sahilbandhe@gmail.com>
2019-02-24 20:12:20 +0530

    responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 import classes from './Layout.css';
-const layout = (props) => (
-    <Aux>
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-        <div>ToolBar , SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-    </Aux>
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
 
-);
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
-export default layout;
+export default Layout;
